7251bda17279b9b190c98266ba8ed188353e8d23
RocknCoder <rockncoder@gmail.com>
2015-08-09 06:38:04 -0700

    adds some test routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MEAN Corp' });
 });
 
+router.get('/about', function(req, res, next){
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contacts', function(req, res, next){
+  res.render('index', { title: 'Contacts' });
+});
+
+router.get('/add-contact', function(req, res, next){
+  res.render('index', { title: 'Add Contact' });
+});
 module.exports = router;
