1a955059d878937de5cef4d45be631c7701ea5fc
RocknCoder <rockncoder@gmail.com>
2015-08-10 17:16:59 -0700

    fixes issues with the contacts/contacts api. It is now functioning correctly

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var contactSchema = new mongoose.Schema({
   gender: String,
-  name : {
+  name: {
     first: String,
     last: String,
     title: String
@@ -26,32 +26,54 @@ var contactSchema = new mongoose.Schema({
 
 var ContactModel = mongoose.model('Contacts', contactSchema);
 
-function get(callback){
+function get(callback) {
   ContactModel.find({}, function (err, contacts) {
     callback(err, contacts);
   });
 }
 
-function getById(id, callback){
+function getById(id, callback) {
   ContactModel.findById(id, function (err, contacts) {
     callback(err, contacts);
   });
 }
 
-function post(contact, callback){
-  ContactModel.findOneAndUpdate({id: contact.id}, {
-      name: {
-        first: contact.first,
-        last: contact.last
-      }
-  }, {upsert: true}, function (err, contacts) {
-    callback(err, contacts);
-  });
+function post(contact, callback) {
+  if(!contacts._id){
+    var person = new ContactModel(contact);
+    person.save(function(err){
+      callback(err);
+    });
+  }
 }
+
+function put(contact, callback) {
+  ContactModel.findByIdAndUpdate(
+    contact._id,
+    contact,
+    {
+      new: true,
+      upsert: true
+    },
+    function (err, contacts) {
+      callback(err, contacts);
+    });
+}
+
+function del(id, callback) {
+  ContactModel.findByIdAndRemove(
+    id,
+    function (err, data) {
+      callback(err, data);
+    });
+}
+
 module.exports = function () {
   return {
     getContacts: get,
     getContact: getById,
-    postContact: post
+    postContact: post,
+    putContact: put,
+    deleteContact: del
   };
 }();
--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,33 @@ var db = require('../models/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MEAN Corp' });
+  res.render('layout', { title: 'MEAN Corp' });
 });
 
 router.get('/about', function(req, res, next){
-  res.render('index', { title: 'About' });
+  res.render('layout', { title: 'About' });
 });
 
 router.get('/contacts', function(req, res, next){
   db.Contacts.getContacts(function(err, contacts){
     if(!err){
-      res.render('contacts', { title: 'Contacts', contacts: contacts});
+      res.render('layout', { title: 'Contacts', contacts: contacts});
     }
   });
 });
 
 router.get('/add-contact', function(req, res, next){
-  res.render('index', { title: 'Add Contact' });
+  res.render('layout', { title: 'Add Contact' });
 });
 
 router.get('/edit-contact/:id', function(req, res, next){
   var id = req.params.id;
   db.Contacts.getContact(id, function(err, contact) {
-    res.render('edit-contact', {title: 'Edit Contact', contact: contact});
+    res.render('layout', {title: 'Edit Contact', contact: contact});
   })
 });
 
+///
 router.post('/edit-contact/', function(req, res, next){
   console.log("Edit Contact Post");
   db.Contacts.postContact(req.body, function(err, data){
@@ -45,12 +46,34 @@ router.get('/contacts/contacts', function(req, res, next){
   });
 });
 
+// REST API (/contacts/contacts)
+
 router.get('/contacts/contacts/:id', function(req, res, next){
   var id = req.params.id;
   db.Contacts.getContact(id, function(err, contact) {
     res.json(contact);
-  })
+  });
 });
 
+router.put('/contacts/contacts', function(req, res, next){
+  var contact = req.body.contact;
+  db.Contacts.putContact(contact, function(err, contact) {
+    res.json(contact);
+  });
+});
+
+router.post('/contacts/contacts', function(req, res, next){
+  var contact = req.body.contact;
+  db.Contacts.postContact(contact, function(err, contact) {
+    res.json(contact);
+  });
+});
+
+router.delete('/contacts/contacts/:id', function(req, res, next){
+  var id = req.params.id;
+  db.Contacts.deleteContact(id, function(err, data) {
+    res.json(data);
+  });
+});
 
 module.exports = router;
